101b3ebfdcf61951ca9bece5f21d70fe3b2aec7c
cesar arturo de paz quispe <cesardpaz@iMac-de-cesar.local>
2021-10-03 18:09:41 -0500

    Added links on page

--- build/js/functions.js
+++ build/js/functions.js
@@ -1,8 +1,37 @@
 const btnCart = document.querySelector('.header__main__shopping button');
-
+const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.header__top');
+const minicart = document.querySelector('.minicart');
 btnCart.addEventListener('click', openMinicart);
 
 function openMinicart () {
-    document.querySelector('.minicart').classList.add('active');
-    document.querySelector('.overlay').classList.add('active');
+    minicart.classList.add('active');
+    overlay.classList.add('active');
 }
+
+
+const bntToggle = document.querySelector('.menu_toggle');
+
+bntToggle.addEventListener('click', () => {
+    
+    if(menu.classList.contains('on'))  {
+        menu.classList.remove('on');
+        overlay.classList.remove('active');
+    } else {
+        menu.classList.add('on');
+        overlay.classList.add('active');
+    }
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('on');
+    overlay.classList.remove('active');
+    minicart.classList.remove('active');
+});
+
+window.addEventListener('resize',(event) => {
+    console.log('Scrolling...');
+    menu.classList.remove('on');
+    overlay.classList.remove('active');
+    minicart.classList.remove('active');
+});
